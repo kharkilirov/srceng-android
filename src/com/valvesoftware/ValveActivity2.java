81362f1d9a5cdcb1bf9727f7c827496f615c0101
kharkilirov <kharkilirov@gmail.com>
2024-05-16 13:52:27 +0500

    Update ValveActivity2.java

--- src/com/valvesoftware/ValveActivity2.java
+++ src/com/valvesoftware/ValveActivity2.java
@@ -1,123 +1,107 @@
 package com.valvesoftware;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
-import android.graphics.Point;
-import android.os.Bundle;
-import android.view.Display;
-import java.util.HashMap;
+import android.util.Log;
+
 import java.io.File;
 import java.util.Locale;
-import org.libsdl.app.SDLActivity;
-import me.nillerusr.LauncherActivity;
-import android.content.SharedPreferences;
-import android.content.Context;
-import android.util.Log;
+
 import me.nillerusr.ExtractAssets;
+import me.nillerusr.LauncherActivity;
 
-public class ValveActivity2 { // not activity, i am lazy to change native methods
-	private static Activity mSingleton;
-	public static SharedPreferences mPref;
+public class ValveActivity2 {
+    private static Activity mSingleton;
+    public static SharedPreferences mPref;
 
+    public static native void setArgs(String args);
+    public static native int setenv(String name, String value, int overwrite);
 
-	public static native void setArgs(String args);
-	public static native int setenv(String name, String value, int overwrite);
-	private static native void nativeOnActivityResult(Activity activity, int i, int i2, Intent intent);
+    public static boolean findGameinfo(String path) {
+        File dir = new File(path);
+        if (!dir.isDirectory())
+            return false;
 
-	public static boolean findGameinfo(String path)
-	{
-		File dir = new File(path);
-		if( !dir.isDirectory() )
-			return false;
+        for (File file : dir.listFiles()) {
+            if (file.isDirectory()) {
+                for (File f : file.listFiles()) {
+                    if (f.getName().toLowerCase().equals("gameinfo.txt"))
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
 
-		for( File file : dir.listFiles() )
-		{
-			if( file.isDirectory() )
-			{
-				for( File f : file.listFiles() )
-				{
-					if( f.getName().toLowerCase().equals("gameinfo.txt") )
-						return true;
-				}
-			}
-		}
+    static public boolean isModGameinfoExists(String path) {
+        File dir = new File(path);
+        if (!dir.isDirectory())
+            return false;
 
-		return false;
-	}
+        for (File file : dir.listFiles()) {
+            if (file.isFile() && file.getName().toLowerCase().equals("gameinfo.txt"))
+                return true;
+        }
+        return false;
+    }
 
-	static public boolean isModGameinfoExists(String path)
-	{
-		File dir = new File(path);
-		if( !dir.isDirectory() )
-			return false;
+    static public boolean preInit(Context context, Intent intent) {
+        mPref = context.getSharedPreferences("mod", 0);
+        String gamepath = mPref.getString("gamepath", LauncherActivity.getDefaultDir() + "/srceng");
+        String gamedir = intent.getStringExtra("gamedir");
+        if (gamedir == null || gamedir.isEmpty())
+            gamedir = "csgo";
 
-		for( File file : dir.listFiles() )
-		{
-			if( file.isFile() && file.getName().toLowerCase().equals("gameinfo.txt") )
-				return true;
-		}
+        if (!findGameinfo(gamepath) || !isModGameinfoExists(gamepath + "/" + gamedir))
+            return false;
 
-		return false;
-	}
+        return true;
+    }
 
-	static public boolean preInit(Context context, Intent intent)
-	{
-		mPref = context.getSharedPreferences("mod", 0);
-		String gamepath = mPref.getString("gamepath", LauncherActivity.getDefaultDir() + "/srceng");
-		String gamedir = intent.getStringExtra("gamedir");
-		if( gamedir == null || gamedir.isEmpty() )
-			gamedir = "hl2";
+    static public void initNatives(Context context, Intent intent) {
+        mPref = context.getSharedPreferences("mod", 0);
+        ApplicationInfo appinf = context.getApplicationInfo();
+        String gamepath = mPref.getString("gamepath", LauncherActivity.getDefaultDir() + "/srceng");
 
-		if( !findGameinfo(gamepath) || !isModGameinfoExists(gamepath+"/"+gamedir) )
-			return false;
+        String argv = intent.getStringExtra("argv");
+        String gamedir = intent.getStringExtra("gamedir");
+        String gamelibdir = intent.getStringExtra("gamelibdir");
+        String customVPK = intent.getStringExtra("vpk");
+        Log.v("SRCAPK", "argv=" + argv);
 
-		return true;
-	}
+        if (gamedir == null || gamedir.isEmpty())
+            gamedir = "csgo";
 
-	static public void initNatives(Context context, Intent intent) {
-		mPref = context.getSharedPreferences("mod", 0);
-		ApplicationInfo appinf = context.getApplicationInfo();
-		String gamepath = mPref.getString("gamepath", LauncherActivity.getDefaultDir() + "/srceng");
+        if (argv == null || argv.isEmpty())
+            argv = mPref.getString("argv", "-console");
 
-		String argv = intent.getStringExtra("argv");
-		String gamedir = intent.getStringExtra("gamedir");
-		String gamelibdir = intent.getStringExtra("gamelibdir");
-		String customVPK = intent.getStringExtra("vpk");
-		Log.v("SRCAPK", "argv="+argv);
+        argv = "-game " + gamedir + " " + argv;
 
-		if( gamedir == null || gamedir.isEmpty() )
-			gamedir = "hl2";
+        if (gamelibdir != null && !gamelibdir.isEmpty())
+            setenv("APP_MOD_LIB", gamelibdir, 1);
 
-		if( argv == null || argv.isEmpty() )
-			argv = mPref.getString("argv", "-console");
+        ExtractAssets.extractVPK(context, false);
 
-		argv = "-game "+gamedir+" "+argv;
+        String vpks = context.getFilesDir().getPath() + "/" + ExtractAssets.VPK_NAME;
+        if (customVPK != null && !customVPK.isEmpty())
+            vpks = customVPK + "," + vpks;
 
-		if( gamelibdir != null && !gamelibdir.isEmpty() )
-			setenv( "APP_MOD_LIB", gamelibdir, 1 );
+        Log.v("SRCAPK", "vpks=" + vpks);
 
-		ExtractAssets.extractVPK(context, false);
+        setenv("EXTRAS_VPK_PATH", vpks, 1);
+        setenv("LANG", Locale.getDefault().toString(), 1);
+        setenv("APP_DATA_PATH", appinf.dataDir, 1);
+        setenv("APP_LIB_PATH", appinf.nativeLibraryDir, 1);
 
-		String vpks = context.getFilesDir().getPath()+"/"+ExtractAssets.VPK_NAME;
-		if( customVPK != null && !customVPK.isEmpty() )
-			vpks = customVPK+","+vpks;
+        if (mPref.getBoolean("rodir", false))
+            setenv("VALVE_GAME_PATH", LauncherActivity.getAndroidDataDir(), 1);
+        else
+            setenv("VALVE_GAME_PATH", gamepath, 1);
 
-		Log.v("SRCAPK", "vpks="+vpks);
-
-		setenv( "EXTRAS_VPK_PATH", vpks, 1 );
-		setenv( "LANG", Locale.getDefault().toString(), 1 );
-		setenv( "APP_DATA_PATH", appinf.dataDir, 1);
-		setenv( "APP_LIB_PATH", appinf.nativeLibraryDir, 1);
-
-		if (mPref.getBoolean("rodir", false))
-			setenv( "VALVE_GAME_PATH", LauncherActivity.getAndroidDataDir(), 1 );
-		else
-			setenv( "VALVE_GAME_PATH", gamepath, 1 );
-
-		Log.v("SRCAPK", "argv="+argv);
-		setArgs(argv);
-	}
+        Log.v("SRCAPK", "argv=" + argv);
+        setArgs(argv);
+    }
 }
