6135682b5f81e4e7a00af5e950b6dd0cefdaab6d
kharkilirov <kharkilirov@gmail.com>
2024-05-16 12:21:40 +0200

    Update build-java.sh

--- scripts/build-java.sh
+++ scripts/build-java.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+# Убедимся, что переменная окружения ANDROID_HOME установлена
+if [ -z "$ANDROID_HOME" ]; then
+  echo "ANDROID_HOME не установлен. Установите переменную окружения ANDROID_HOME и повторите попытку."
+  exit 1
+fi
+
+# Убедимся, что переменная окружения JAVA_HOME установлена
+if [ -z "$JAVA_HOME" ]; then
+  echo "JAVA_HOME не установлен. Установите переменную окружения JAVA_HOME и повторите попытку."
+  exit 1
+fi
+
 # Путь к Android SDK и Java
 ANDROID_SDK_ROOT=$ANDROID_HOME
-JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+JAVA_HOME=$JAVA_HOME
 
 # Путь к необходимым инструментам
 AAPT=$ANDROID_SDK_ROOT/build-tools/30.0.3/aapt
 DX=$ANDROID_SDK_ROOT/build-tools/30.0.3/dx
 APKBUILDER=$ANDROID_SDK_ROOT/tools/lib/apkbuilder.jar
 ANDROID_JAR=$ANDROID_SDK_ROOT/platforms/android-30/android.jar
+SIGNAPK_JAR=$ANDROID_SDK_ROOT/build-tools/30.0.3/lib/signapk.jar
+TESTKEY_X509_PEM=$ANDROID_SDK_ROOT/build-tools/30.0.3/lib/testkey.x509.pem
+TESTKEY_PK8=$ANDROID_SDK_ROOT/build-tools/30.0.3/lib/testkey.pk8
 
 # Имя сборки
 NAME=csgo
@@ -34,4 +49,4 @@ $AAPT package -f -M AndroidManifest.xml -I $ANDROID_JAR -F bin/$NAME.apk.unalign
 java -jar $APKBUILDER bin/$NAME.apk -u -nf libs/ -rj libs -f bin/classes.dex -z bin/$NAME.apk.unaligned
 
 # Подпись APK файла
-java -jar $ANDROID_SDK_ROOT/build-tools/30.0.3/lib/signapk.jar $ANDROID_SDK_ROOT/build-tools/30.0.3/lib/testkey.x509.pem $ANDROID_SDK_ROOT/build-tools/30.0.3/lib/testkey.pk8 bin/$NAME.apk bin/$NAME-signed.apk
+java -jar $SIGNAPK_JAR $TESTKEY_X509_PEM $TESTKEY_PK8 bin/$NAME.apk bin/$NAME-signed.apk
